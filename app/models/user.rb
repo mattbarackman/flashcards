243873d7bac433cea0c0efaecbcebe506c802438
Apprentice <apprentice@devbootcamp.com>
2013-06-07 14:00:13 -0700

    add user autho

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,22 @@
 class User < ActiveRecord::Base
+
+	before_create :encrypt_password
+
 	validates :username, :password, presence: true
 	validates :username, uniqueness: true
 
   has_many :decks, :through => :rounds
   has_many :rounds
+
+  def self.authenticate(username, password)
+  	user = self.find_by_username(username)
+  	temp_encryption = Digest::SHA1.hexdigest("#{password}")
+  	user.password == temp_encryption ? user : nil
+  end
+
+	def encrypt_password
+ 		encrypted_password = Digest::SHA1.hexdigest("#{password}")
+		self.password = encrypted_password
+	end
+
 end
