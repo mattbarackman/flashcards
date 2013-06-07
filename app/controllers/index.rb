2ea54a3b72fb53280e2810c722f659293dec179e
Apprentice <apprentice@devbootcamp.com>
2013-06-07 15:57:46 -0700

    add user login, logout, sign-up

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,4 +1,35 @@
+enable :sessions
+
 get '/' do
-  # Look in app/views/index.erb
-  erb :index
+	erb :index
+end
+
+# create user
+post '/users' do
+	User.create(params[:user])
+	redirect '/'
+end
+
+# create session
+post '/sessions' do
+	@user = User.authenticate(params[:user])
+	if @user
+		session["user"] = @user.id
+		"HEY YOU LOGGED IN"
+		redirect '/'
+	else
+		password = nil
+		"YOU FAILED TO LOG IN"
+	end
+end
+
+
+get '/logout' do
+	session.clear
+	puts "I logged out"
+	redirect '/'
+end
+
+get '/decks' do
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,22 +1,25 @@
 class User < ActiveRecord::Base
 
-	before_create :encrypt_password
+  before_create :encrypt_password
 
-	validates :username, :password, presence: true
-	validates :username, uniqueness: true
+  validates :username, :password, presence: true
+  validates :username, uniqueness: true
 
   has_many :decks, :through => :rounds
   has_many :rounds
 
-  def self.authenticate(username, password)
-  	user = self.find_by_username(username)
-  	temp_encryption = Digest::SHA1.hexdigest("#{password}")
-  	user.password == temp_encryption ? user : nil
-  end
+  def self.authenticate(args)
+    user = self.find_by_username(args[:username])
+    if user
+      temp_encryption = Digest::SHA1.hexdigest("#{args[:password]}")
+      user.password == temp_encryption ? user : nil
+   end
+ end
 
-	def encrypt_password
- 		encrypted_password = Digest::SHA1.hexdigest("#{password}")
-		self.password = encrypted_password
-	end
+def encrypt_password
+  encrypted_password = Digest::SHA1.hexdigest("#{password}")
+  self.password = encrypted_password
+  password = nil
+end
 
 end
