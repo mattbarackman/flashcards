1892fa6cec1c2bcf557d2002d26629abae6afb98
Apprentice <apprentice@devbootcamp.com>
2013-06-08 16:44:37 -0700

    Deleted the case sensitive validations

--- public/js/application.js
+++ public/js/application.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   var order;
-  var card_counter = 40;
+  var card_counter = 0;
   var user_id;
 
   function questionRequest(){ 
@@ -21,15 +21,12 @@ $(document).ready(function() {
       url: "/rounds/card_order",
       method: "GET"
     }).done(function(response){
-      order = $.parseJSON(response);
-      console.log(order);
+      order = $.parseJSON(response); //set order to shuffled array of indices [5, 1, 3, ...]
     }).done(
       questionRequest
     );
 
-
   $('.question').on('submit','form',function(e){
-    console.log("in here");
     e.preventDefault();
     $.ajax({
       url: "/rounds/submit_guess",
@@ -41,23 +38,4 @@ $(document).ready(function() {
       setTimeout(questionRequest, 2000);
       });
     });
-
-
-
-  // var card_index
-  // $('.question').on('submit', 'form', function(e){
-  //   console.log("this should not run");
-  //   e.preventDefault();
-  //   $.ajax({
-  //     url: "/rounds",
-  //     method: "POST",
-  //     data: $(this).serialize()
-  //   }).done(function(response){
-  //     console.log(response);
-  //     $('.question').html(response.answer);
-  //     setTimeout(function(){
-  //       $('.question').html(response.question);
-  //     }, 2000);
-  //   });
-  // });
 });
