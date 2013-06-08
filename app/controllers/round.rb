0fffbf9307890db2c6c38244fdce1d64b1a64907
DBC-Apprentice <dbc@dbc.com>
2013-06-07 17:37:07 -0700

    refactor js but still broken

--- app/controllers/round.rb
+++ app/controllers/round.rb
@@ -12,12 +12,28 @@ def to_js(card)
 end
 
 get '/rounds' do
-  round = Round.create(user_id: session[:id], deck_id: params[:deck_id])
-  session[:cards] = Deck.find(params[:deck_id]).cards.shuffle
-  session[:round_id] = round.id
-  @card = Card.find(session[:cards].pop)
+  deck_id = params[:deck_id]
+  round = Round.create(user_id: session[:id], deck_id: @deck_id)
+  session[:deck_id] = deck_id  
   erb :round
 end
+
+get '/rounds/card_order' do
+
+  # session[:round_id] = round.id
+  # @card = Card.find(session[:cards].pop)
+  # deck_id = Round.find(session[:round_id]).deck_id
+  p Deck.find(session[:deck_id]).cards.map { |card| card.id }.shuffle.to_json
+end
+
+get '/rounds/get_question' do
+  p params
+
+end
+
+get '/rounds/submit_guess' do
+end
+
 
 post '/rounds' do
   p session
@@ -28,7 +44,7 @@ post '/rounds' do
   Guess.create(round_id: session[:round_id], card_id: card.id, 
                is_correct: card.correct?(params[:guess]))
 
-  @card = session[:cards].pop
+  @card = Card.find(session[:cards].pop)
   if @card
     {question: (erb :_question, :layout => false),
     answer: (erb :_answer, :layout => false)}.to_json
--- public/js/application.js
+++ public/js/application.js
@@ -1,13 +1,56 @@
 $(document).ready(function() {
+  var order;
+  var card_counter = 0;
 
-  $('question').on('submit', 'form', function(e){
-    e.preventDefault();
-    $.ajax({
-      url: "/rounds",
-      method: "POST",
-      data: $(this).serialize()
+   function questionRequest(){ $.ajax({
+        url: "/rounds/get_question",
+        method: "GET",
+        data: "hello"
+      }).done(function(response){
+        console.log(response);
+      });
+    }
+
+  $.ajax({
+      url: "/rounds/card_order",
+      method: "GET"
     }).done(function(response){
-      $('.question').html(response);
+      order = response;
+      console.log(order);
+    }).done({
+      questionRequest();
     });
-  });
+
+
+
+
+
+  // $('.question').on('submit','form',function(e){
+  //   e.preventDefault();
+  //   $.ajax({
+  //     url: "/rounds/submit_guess",
+  //     method: "POST",
+  //     data: $(this).serialize()
+  //   }).done(function(){
+  //     card_counter++;
+  //   })
+  // }
+
+
+  // var card_index
+  // $('.question').on('submit', 'form', function(e){
+  //   console.log("this should not run");
+  //   e.preventDefault();
+  //   $.ajax({
+  //     url: "/rounds",
+  //     method: "POST",
+  //     data: $(this).serialize()
+  //   }).done(function(response){
+  //     console.log(response);
+  //     $('.question').html(response.answer);
+  //     setTimeout(function(){
+  //       $('.question').html(response.question);
+  //     }, 2000);
+  //   });
+  // });
 });
